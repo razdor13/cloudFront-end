7ba0739cedfa298c757863c565c8a60ca768436a
razdor <oneamaday@gmail.com>
2024-08-01 13:26:00 +0300

    fix width from upload btn

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import {Inter} from "next/font/google";
 import "./globals.css";
 import styles from "./Home.module.scss";
 import HeaderMy from "@/components/Header";
-import { AntdRegistry } from '@ant-design/nextjs-registry';
+import {AntdRegistry} from "@ant-design/nextjs-registry";
 const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
@@ -19,14 +19,13 @@ export default function RootLayout({
     return (
         <AntdRegistry>
             <html lang="en">
-            <body className={inter.className}>
-            <HeaderMy/>
-            <div className={styles.main}>
-          <div className={styles.layout}>{children}</div>
-        </div>
-            </body>
-        </html>
+                <body className={inter.className}>
+                      <HeaderMy />
+                    <div className={styles.main}>
+                    <div className={styles.layout}>{children}</div>
+                    </div>
+                </body>
+            </html>
         </AntdRegistry>
-        
     );
 }
